f20f1fa7f3a86d26234e4c57f5ed8a3f3f526b58
Torben Reetz <torben.reetz@sinnerschrader.com>
2016-12-15 14:37:25 +0100

    add all the math

---
+++ alg.js
@@ -0,0 +1,7 @@
+function alg(a,b,c,d) {
+  return (((0.25 * a) / 3) + ((0.25 * b) / 3) + 0.25 * c + 0.25 * d) * 0.25
+}
+
+console.log(alg(2, 1.5, 0.44, 0.42));
+console.log(alg(1, 3, 0.56, 0.61));
+console.log(alg(2.5, 2, 0.58, 0.5));
